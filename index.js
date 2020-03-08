77f830773fae003c96c548725ea24ccd137e91eb
robertdbjohnson <joh16151@byui.edu>
2020-03-07 23:20:04 -0700

    Week09 stuff

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ express()
   .use(express.static('public'))
   .set('views', 'views')
   .set('view engine', 'ejs')
-  .get("/getRate", function(req, res) {
-    res.render("getRate")
-  })
+  .get("/getRate", calcRate)
+  
   //.get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function calcRate(req, res) {
+  const mail = req.query.mail;
+  const weight = Number(req.query.weight);
+  const params = {mail: mail, weight: weight};
+  res.render("getRate", params);
+}
