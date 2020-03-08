d0c84633a3af0a8a93d1f31501f8c296d29932b9
robertdbjohnson <joh16151@byui.edu>
2020-03-07 21:54:43 -0700

    A lot of random stuff

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static('public'))
   .set('views', 'views')
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
