42dcaab532aa70895a47b9d8668aecf1aee96828
robertdbjohnson <joh16151@byui.edu>
2020-03-07 22:05:17 -0700

    A lot of random stuff

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ express()
   .use(express.static('public'))
   .set('views', 'views')
   .set('view engine', 'ejs')
+  .get("/form", function(req, res) {
+    res.render("getRate")
+  })
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
