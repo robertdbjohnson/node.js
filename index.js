fc750af7e620562fd531282c4f206acc4a1a51e0
robertdbjohnson <joh16151@byui.edu>
2020-03-07 22:09:52 -0700

    Week09 stuff

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ express()
   .use(express.static('public'))
   .set('views', 'views')
   .set('view engine', 'ejs')
-  .get("/form", function(req, res) {
+  .get("/getRate", function(req, res) {
     res.render("getRate")
   })
   //.get('/', (req, res) => res.render('pages/index'))
